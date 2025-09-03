b0f1af58f8f47a681c6d02159afd341b95714b78
Mike Jones, II <micujonesii@gmail.com>
2025-09-03 14:33:46 -0400

    Set global font

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import './index.css';
+import App from './App.jsx';
+
+const theme = createTheme({
+    typography: {
+        fontFamily: 'DM Sans',
+    },
+});
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </StrictMode>
+);
